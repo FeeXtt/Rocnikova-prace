3bd7766b31b11b6f8ba6b73f4c90547e46671bf7
FeeXtt <115619204+FeeXtt@users.noreply.github.com>
2025-04-02 00:32:45 +0200

    bind system
    nedodelany

---
+++ scripts/UI/settings/settings.gd
@@ -0,0 +1,14 @@
+extends Control
+
+
+func _on_audio_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/UI/settings/audio_setting.tscn")
+
+func _on_keyboard_setting_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/UI/settings/keybind_settings.tscn")
+	#get_tree().change_scene_to_file()
+
+
+func _on_controller_settings_button_pressed() -> void:
+	pass
+	#get_tree().change_scene_to_file()
---
+++ scripts/player/player.gd
@@ -0,0 +1,97 @@
+extends CharacterBody2D
+
+class_name Player
+
+const BULLET = preload("res://scenes/player/bullet.tscn")
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+const MAX_JUMP_HOLD_TIME = 0.5
+const BASE_JUMP_FORCE = -300.0
+const MAX_JUMP_FORCE = -500.0
+const DOUBLE_JUMP_FORCE = -300.0
+
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var is_jumping = false
+var is_holding_jump = false
+var is_shooting = false
+var can_double_jump = false
+var jump_timer = 0.0
+var playerposition
+var last_direction = 1
+
+var animation_player: AnimationPlayer
+var sprite: Sprite2D
+
+func _ready():
+	animation_player = $AnimationPlayer
+	sprite = $Sprite2D
+	GameManager.player = self
+	GameManager.reload_player()
+
+#MOVEMENT
+func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	else:
+		can_double_jump = true
+	var direction = Input.get_axis("MoveLeft", "MoveRight")
+	if direction != 0 and not is_holding_jump:
+		velocity.x = direction * SPEED
+		last_direction = sign(direction)
+		sprite.flip_h = direction < 0
+		if not animation_player.is_playing() or animation_player.current_animation != "walk":
+			animation_player.play("walk")
+	elif not is_holding_jump:
+		if Input.is_action_just_pressed("fire"):
+			animation_player.play("fire")
+			
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		if not animation_player.is_playing() or animation_player.current_animation != "idle" && animation_player.current_animation != "fire":
+			animation_player.play("idle")
+	
+	# Handle jump input
+	if Input.is_action_just_pressed("jump"):
+		if is_on_floor():
+			is_jumping = true
+			jump_timer = 0.0
+		elif can_double_jump:
+			velocity.y = DOUBLE_JUMP_FORCE
+			can_double_jump = false
+			
+	
+	if Input.is_action_pressed("jump") and is_jumping:
+		if jump_timer > 0.1:
+			velocity.x = 0
+		
+		if not is_holding_jump:
+			is_holding_jump = true
+			if not animation_player.is_playing() or animation_player.current_animation != "beforejump":
+				animation_player.play("beforejump")
+		
+		if jump_timer < MAX_JUMP_HOLD_TIME:
+			jump_timer += delta
+	
+	if Input.is_action_just_released("jump") and is_jumping:
+		is_holding_jump = false
+		if not animation_player.is_playing() or animation_player.current_animation != "afterjump":
+			animation_player.play("afterjump")
+		
+		var jump_strength = lerp(BASE_JUMP_FORCE, MAX_JUMP_FORCE, jump_timer / MAX_JUMP_HOLD_TIME)
+		velocity.y = jump_strength
+		is_jumping = false
+
+	move_and_slide()
+
+#SHOOTING
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("fire"):
+		is_shooting = true
+		var bullet_instance = BULLET.instantiate()
+		get_tree().root.add_child(bullet_instance)
+		bullet_instance.global_position = position
+		bullet_instance.set_direction(last_direction)
+		is_shooting = false
+
+func die():
+	print("die")
+	GameManager.respawn_player()
